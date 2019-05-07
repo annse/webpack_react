e7e05f46e4b0245cf0559e200e7e077a41edd384
annse <hanna.prog@gmail.com>
2019-05-07 05:44:38 +0300

    init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../styles/App.css';
+
+export default class App extends Component {
+
+    render(){
+
+        return(
+            <div>
+                <h1>Hello, ReactJS!!</h1>
+            </div>
+        );
+    }
+}
